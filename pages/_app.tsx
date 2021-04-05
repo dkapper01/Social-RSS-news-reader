38b3e8766bb6a94e6a0ecc6ba5a7db617e885b3e
dkapper01 <dkapper01@gmail.com>
2021-04-05 14:48:53 -0400

    update code base

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../utils/apolloClient';
+// import 'tailwindcss/tailwind.css';
+// import '../styles/index.css';
+
+export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
